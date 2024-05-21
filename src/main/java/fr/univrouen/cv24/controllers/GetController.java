619a9571b29d42650a480751b054436068adade8
miraTR24 <tirecheamira@gmail.com>
2024-05-21 19:47:25 +0200

    add details page

--- src/main/java/fr/univrouen/cv24/controllers/GetController.java
+++ src/main/java/fr/univrouen/cv24/controllers/GetController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ import fr.univrouen.cv24.model.Cv24ResumeList;
 import fr.univrouen.cv24.services.Cv24Service;
 import fr.univrouen.cv24.util.Fichier;
 import fr.univrouen.cv24.xmlValidation.xmlConvert;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class GetController {
@@ -72,11 +74,8 @@ public class GetController {
 		return "help";
 	}
 	
-	@RequestMapping("/*")
-	public String notFound() {
-		return "notFound";
-	}
-//recuperer la liste des cv en xml
+
+
 	@GetMapping(value = "/cv24/resume/xml", produces = MediaType.APPLICATION_XML_VALUE)
 	 @ResponseBody
     public Cv24ResumeList getAllCVResumes() {
